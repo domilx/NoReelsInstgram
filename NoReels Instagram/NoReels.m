645f794aea3e22edd980373c73fe1edde1f9353a
domilx <domenico2727@gmail.com>
2025-02-23 19:25:36 -0500

    Remove commented-out code for FLEXManager (debug) explorer display

--- NoReels Instagram/NoReels.m
+++ NoReels Instagram/NoReels.m
@@ -26,10 +26,10 @@
 + (void)load {
     NSLog(@"[NoReels] Loaded");
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)),
-                   dispatch_get_main_queue(), ^{
-        [FLEXManager.sharedManager showExplorer];
-    });
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)),
+//                   dispatch_get_main_queue(), ^{
+//        [FLEXManager.sharedManager showExplorer];
+//    });
     
     dispatch_async(dispatch_get_main_queue(), ^{
         [[self sharedInstance] startReplaceTimer];
